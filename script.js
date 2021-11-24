ada24acec7828b31ee154447122af8ad89c8ee80
danisltpi <lcdns@protonmail.com>
2021-11-24 20:57:02 +0100

    initial commit + setup

---
+++ script.js
@@ -0,0 +1,40 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(operator, x, y) {
+  return operator(x, y);
+}
+
+buttonSymbols = [
+  "7",
+  "8",
+  "9",
+  "÷",
+  "4",
+  "5",
+  "6",
+  "×",
+  "1",
+  "2",
+  "3",
+  "-",
+  ".",
+  "0",
+  "=",
+  "+",
+];
+
+buttonSymbols.forEach((i) => console.log(i));
